05d030b69483e794d4cfbd41c2386dab32de32ef
niyatN <niyatpatel2410@gmail.com>
2019-02-06 12:52:39 +0530

    basic schema and mutation is added

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// for parsing graphql query
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 const app = express();
+
+// middleware
 app.use(bodyParser.json());
 
+app.use('/graphql',graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String):String
+        }
+        schema{
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    // resolvers
+    rootValue: {
+        events:()=>{
+            return ['Cooking','for','Coding','or', 'vise', 'vresa'];
+        },
+        createEvent: (args)=>{
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql:true
+}))
+
 app.get('/', (req, res, next)=>{
-    res.send('Om Shri ganeshah namah...')
+    res.send('1+1=10')
 })
 
 
